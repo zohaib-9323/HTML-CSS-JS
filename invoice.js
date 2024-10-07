aecf916cb65bc5e5882c7abef665181ffa2ce61d
Zohaib <zohaib.asghar1071@gmail.com>
2024-10-07 17:31:13 +0500

    final commit message

--- invoice.js
+++ invoice.js
@@ -16,50 +16,77 @@ date.innerHTML=`${currentdate}`;
 if (!cart || cart.length === 0) {
   alert("Your cart is empty!");
 } else {
-  fetch("products.json")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((listproducts) => {
-      let totalAmount = 0;
-      tbodyEl.innerHTML = "";
-      cart.forEach((item) => {
-        let productInfo = listproducts.find((product) => product.id == item.id_product);
-        if (productInfo) {
-          let itemTotal = productInfo.price * item.quantity;
-          totalAmount += itemTotal;
-          tbodyEl.innerHTML += `
-            <tr>
-                <td style="text-align: left; padding-left:25px">
-                    ${productInfo.name}
-                </td>
-                <td style ="text-align: center;">
-                    ${productInfo.price}
-                </td>
-                <td style ="text-align: center;">
-                    ${item.quantity}
-                </td>
-                <td style="text-align: center;">
-                    Rs.${itemTotal.toFixed(2)}
-                </td>
-            </tr>`;
-        } else {
-          console.error(`Product with ID ${item.id_product} not found.`);
-        }
-      });
-        salestax = (totalAmount * 16) / 100;
-        subtotal.innerHTML = "Rs."+totalAmount;
-        saletax.innerHTML = "Rs." +salestax;
-        disc=(totalAmount+salestax)*0.04;
-        Discounts.innerHTML="Rs." + (disc.toFixed(2));
-        total.innerHTML = "Rs."+ ((totalAmount + salestax+400)-disc);
-    })
-    .catch((error) => {
-      console.error("Error fetching the products:", error);
-    });
+  let totalAmount=0;
+  tbodyEl.innerHTML="";
+  cart.forEach((item) => {
+    let itemTotal = item.price * item.quantity;
+    totalAmount += itemTotal;
+    tbodyEl.innerHTML += `
+    <tr>
+                 <td style="text-align: left; padding-left:25px">
+                     ${item.name}
+                 </td>
+                 <td style ="text-align: center;">
+                     ${item.price}
+                 </td>
+                 <td style ="text-align: center;">
+                     ${item.quantity}
+                 </td>
+                 <td style="text-align: center;">
+                     Rs.${itemTotal.toFixed(2)}
+                 </td>
+             </tr>`;
+  });
+  salestax = (totalAmount * 16) / 100;
+  subtotal.innerHTML = "Rs."+totalAmount;
+  saletax.innerHTML = "Rs." +salestax;
+  disc=(totalAmount+salestax)*0.04;
+  Discounts.innerHTML="Rs." + (disc.toFixed(2));
+  total.innerHTML = "Rs."+ ((totalAmount + salestax+400)-disc);
+  // fetch("products.json")
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error("Network response was not ok");
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((listproducts) => {
+  //     let totalAmount = 0;
+  //     tbodyEl.innerHTML = "";
+  //     cart.forEach((item) => {
+  //       let productInfo = listproducts.find((product) => product.id == item.id_product);
+  //       if (productInfo) {
+  //         let itemTotal = productInfo.price * item.quantity;
+  //         totalAmount += itemTotal;
+  //         tbodyEl.innerHTML += `
+  //           <tr>
+  //               <td style="text-align: left; padding-left:25px">
+  //                   ${productInfo.name}
+  //               </td>
+  //               <td style ="text-align: center;">
+  //                   ${productInfo.price}
+  //               </td>
+  //               <td style ="text-align: center;">
+  //                   ${item.quantity}
+  //               </td>
+  //               <td style="text-align: center;">
+  //                   Rs.${itemTotal.toFixed(2)}
+  //               </td>
+  //           </tr>`;
+  //       } else {
+  //         console.error(`Product with ID ${item.id_product} not found.`);
+  //       }
+  //     });
+  //       salestax = (totalAmount * 16) / 100;
+  //       subtotal.innerHTML = "Rs."+totalAmount;
+  //       saletax.innerHTML = "Rs." +salestax;
+  //       disc=(totalAmount+salestax)*0.04;
+  //       Discounts.innerHTML="Rs." + (disc.toFixed(2));
+  //       total.innerHTML = "Rs."+ ((totalAmount + salestax+400)-disc);
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error fetching the products:", error);
+  //   });
 }
 back.addEventListener("click", function () {
     window.location.href = "index.html";
--- script.js
+++ script.js
@@ -12,91 +12,63 @@ const tbodyEl = document.querySelector("tbody");
 let carttab = document.querySelector(".carttab");
 let subtotal = document.querySelector(".subTotal");
 let total = document.querySelector(".Total");
-let saletax =document.querySelector(".Saletax");
+let saletax = document.querySelector(".Saletax");
 let clear = document.querySelector(".clear");
 let count = document.querySelector(".count");
 let dvcharge = document.querySelector(".DVcharge");
 let discount = document.querySelector(".Disc");
-const modal = document.getElementById('confirmModal');
-const confirmBtn = document.getElementById('confirmBtn');
-const cancelBtn = document.getElementById('cancelBtn');
+const modal = document.getElementById("confirmModal");
+const confirmBtn = document.getElementById("confirmBtn");
+const cancelBtn = document.getElementById("cancelBtn");
 
 let salestax = 0;
 let listproducts = [];
 let cart = [];
 
-
-
 container.addEventListener("click", (event) => {
   const positionClick = event.target;
 
-  // if(positionClick.classList.contains("Addcart")|| positionClick.classList.contains("plus")|| positionClick.classList.contains("minus")||positionClick.classList.contains("Quantity")||positionClick.classList.contains("test")  ){
-  //   console.log('called')
-
-  // }else{
-  //   if(body.classList.contains("showcart")){
-  //     body.classList.remove("showcart");
-
-  //   }
-  // }
-
-  // }
-   if(positionClick.classList.contains("test")||positionClick.classList.contains("count")){
-    if(body.classList.contains("showcart")){
-      body.classList.add("showcart");
-    }
-      
-   }
-   else if (
-    positionClick.classList.contains("Addcart") || 
-    positionClick.classList.contains("quantity-selector") ||
-    positionClick.classList.contains("plus") ||
-    positionClick.classList.contains("minus") || 
-    positionClick.classList.contains("Quantity")
+  if (
+    positionClick.classList.contains("cart") ||
+    positionClick.classList.contains("count")
   ) {
-   
     if (body.classList.contains("showcart")) {
       body.classList.add("showcart");
     }
-  } else if (body.classList.contains("showcart")) {
+  } else if (
+    positionClick.classList.contains("Addcart") ||
+    positionClick.classList.contains("quantity-selector") ||
+    positionClick.classList.contains("plus") ||
+    positionClick.classList.contains("minus") ||
+    positionClick.classList.contains("Quantity")
+  ) {
+    if (body.classList.contains("showcart")) {
+      body.classList.add("showcart");
+    }
+  } else {
     body.classList.remove("showcart");
   }
-  //  else {
-  //   // console.log('called')
-  //   // if(body.classList.contains("showcart")){
-  //   //   debugger
-      
-  //   //   body.classList.remove("showcart");
-
-  //   }
-    // Hide the cart if any other area is clicked and cart is visible
-    
-  // }
 });
 
 iconcart.addEventListener("click", () => {
-  if(cart.length>0)
-  {
+  if (cart.length > 0) {
     body.classList.add("showcart");
   }
-  
 });
 
 close.addEventListener("click", () => {
   body.classList.remove("showcart");
 });
+
 const adddatatohtml = () => {
   productlisthtml.innerHTML = "";
 
   if (listproducts.length > 0) {
     listproducts.forEach((product) => {
-      
-      let cartItem = cart.find(item => item.id_product === product.id);
+      let cartItem = cart.find((item) => item.id_product === product.id);
 
-      
-      let quant = cartItem ? cartItem.quantity : 0; 
+      let quant = cartItem ? cartItem.quantity : 0;
 
-      
       let newproduct = document.createElement("div");
       newproduct.classList.add("items");
       newproduct.dataset.id = product.id;
@@ -107,7 +79,9 @@ const adddatatohtml = () => {
           <h2>${product.name}</h2>
           <div class="price">Rs.${product.price}</div>
           <button class="Addcart">Add to cart</button>
-          <div class="quantity-selector" style="display: ${quant > 0 ? 'flex' : 'none'};">
+          <div class="quantity-selector" style="display: ${
+            quant > 0 ? "flex" : "none"
+          };">
             <button class="minus">–</button>
             <span class="Quantity">${quant}</span>
             <button class="plus">+</button>
@@ -116,15 +90,13 @@ const adddatatohtml = () => {
         
       `;
 
-      
       productlisthtml.appendChild(newproduct);
 
-      
       if (quant > 0) {
         let quantitySelector = newproduct.querySelector(".quantity-selector");
         let addCartButton = newproduct.querySelector(".Addcart");
-        quantitySelector.style.display = "flex"; 
-        addCartButton.style.display = "none"; 
+        quantitySelector.style.display = "flex";
+        addCartButton.style.display = "none";
         addCartButton.style.marginLeft = "42.5px";
       }
     });
@@ -134,45 +106,30 @@ productlisthtml.addEventListener("click", (event) => {
   let positionClick = event.target;
   let productCard = positionClick.closest(".items");
   if (positionClick.classList.contains("Addcart")) {
-    iconcart.style.color="black";
-    count.style.display ="inline";
+    iconcart.style.color = "black";
+    count.style.display = "inline";
     let id_product = productCard.dataset.id;
     let quantityInput = productCard.querySelector(".Quantity");
     let currentQuantity = parseInt(quantityInput.innerHTML, 10);
     quantityInput.innerHTML = currentQuantity + 1;
     addtocart(id_product, 1);
-    clear.disabled=false;
-    generateButton.disabled=false;
+    clear.disabled = false;
+    generateButton.disabled = false;
     let quantitySelector = productCard.querySelector(".quantity-selector");
     let addCartButton = productCard.querySelector(".Addcart");
     addCartButton.style.display = "none";
     quantitySelector.style.display = "flex";
-  }
-  // else if (positionClick.classList.contains("confirm")) {
-  //   // let id_product = productCard.dataset.id;
-  //   // let quantityInput = productCard.querySelector(".Quantity").innerHTML;
-  //   // addtocart(id_product, parseInt(quantityInput, 10));
-  //   let quantitySelector = productCard.querySelector(".quantity-selector");
-  //   let addCartButton = productCard.querySelector(".Addcart");
-  //   let quantityInputField = productCard.querySelector(".Quantity");
-  //   quantityInputField.innerHTML = 1;
-  //   quantitySelector.style.display = "none";
-  //   addCartButton.style.display = "block";
-  //   addCartButton.style.marginLeft = "42.5px";
-  // }
-  else if (
+  } else if (
     positionClick.classList.contains("plus") ||
     positionClick.classList.contains("minus")
   ) {
     let quantityInput = productCard.querySelector(".Quantity");
     let currentQuantity = parseInt(quantityInput.innerHTML, 10);
-    
+
     if (positionClick.classList.contains("plus")) {
       quantityInput.innerHTML = currentQuantity + 1;
       let id_product = productCard.dataset.id;
       addtocart(id_product, 1);
-
-      
     } else if (
       positionClick.classList.contains("minus") &&
       currentQuantity > 1
@@ -180,17 +137,19 @@ productlisthtml.addEventListener("click", (event) => {
       quantityInput.innerHTML = currentQuantity - 1;
       let id_product = productCard.dataset.id;
       addtocart(id_product, -1);
-    } else if (positionClick.classList.contains("minus") && currentQuantity === 1) {
+    } else if (
+      positionClick.classList.contains("minus") &&
+      currentQuantity === 1
+    ) {
       let id_product = productCard.dataset.id;
       removecartitem(id_product);
       let quantitySelector = productCard.querySelector(".quantity-selector");
       let addCartButton = productCard.querySelector(".Addcart");
-      quantityInput.innerHTML = 0; 
-      quantitySelector.style.display = "none"; 
+      quantityInput.innerHTML = 0;
+      quantitySelector.style.display = "none";
       addCartButton.style.display = "inline-block";
-      clear.disabled=true; 
-      generateButton.disabled=true;
-      
+      clear.disabled = true;
+      generateButton.disabled = true;
     }
   }
 });
@@ -201,22 +160,28 @@ const removecartitem = (product_id) => {
   );
 
   if (positionThisProductInCart >= 0) {
-    cart.splice(positionThisProductInCart, 1); 
+    cart.splice(positionThisProductInCart, 1);
   }
 
   addcarttomemory();
-  addcarttohtml(); 
-  updateCartCount(); 
+  addcarttohtml();
+  updateCartCount();
 };
 const addtocart = (product_id, quantity) => {
   product_id = parseInt(product_id, 10);
   let positionThisProductInCart = cart.findIndex(
     (value) => value.id_product === product_id
   );
+  let productInfo = listproducts.find(
+    (product) => product.id == product_id
+  );
   if (positionThisProductInCart < 0) {
-    cart.push({
+    cart.push({                 //cart initialising
       id_product: product_id,
       quantity: quantity,
+      name: productInfo.name,
+      price: productInfo.price,
+      image: productInfo.image,
     });
   } else {
     cart[positionThisProductInCart].quantity += quantity;
@@ -229,16 +194,14 @@ const addtocart = (product_id, quantity) => {
 
 const addcarttomemory = () => {
   localStorage.setItem("cart", JSON.stringify(cart));
-  
 };
 const updateCartCount = () => {
-  let totalItems = cart.length; 
-  if(totalItems==0)
-  {
-    count.style.display="none";
-    iconcart.style.color="grey";
+  let totalItems = cart.length;
+  if (totalItems == 0) {
+    count.style.display = "none";
+    iconcart.style.color = "grey";
   }
-  iconcartspan.textContent = totalItems; 
+  iconcartspan.textContent = totalItems;
 };
 const addcarttohtml = () => {
   listcarthtml.innerHTML = "";
@@ -284,20 +247,21 @@ const addcarttohtml = () => {
 
       listcarthtml.appendChild(newItem);
       subtotal.innerHTML = "Rs." + totalAmount;
-      salestax=(totalAmount*16/100);
-      discount.innerHTML ="Rs." + (((totalAmount+salestax)*4)/100);
-      saletax.innerHTML ="Rs." + salestax;
+      salestax = (totalAmount * 16) / 100;
+      discount.innerHTML = "Rs." + ((totalAmount + salestax) * 4) / 100;
+      saletax.innerHTML = "Rs." + salestax;
       dvcharge.innerHTML = "Rs.400";
-      total.innerHTML ="Rs." +((totalAmount+salestax+400)-(((totalAmount+salestax)*4)/100));
-
+      total.innerHTML =
+        "Rs." +
+        (totalAmount + salestax + 400 - ((totalAmount + salestax) * 4) / 100);
     });
   }
   if (cart.length == 0) {
     subtotal.innerHTML = "";
-    saletax.innerHTML ="";
-    dvcharge.innerHTML=""
+    saletax.innerHTML = "";
+    dvcharge.innerHTML = "";
     total.innerHTML = "";
-    discount.innerHTML="";
+    discount.innerHTML = "";
   }
 };
 listcarthtml.addEventListener("click", (event) => {
@@ -324,16 +288,15 @@ const changeQuantityCart = (product_id, type) => {
         break;
       case "minus":
         let changeQuantity = cart[positionItemInCart].quantity - 1;
-        
+
         if (changeQuantity > 0) {
           cart[positionItemInCart].quantity = changeQuantity;
           adddatatohtml();
         } else {
           cart.splice(positionItemInCart, 1);
-          clear.disabled=true;
-          generateButton.disabled=true;
+          clear.disabled = true;
+          generateButton.disabled = true;
           adddatatohtml();
-
         }
         break;
     }
@@ -342,21 +305,19 @@ const changeQuantityCart = (product_id, type) => {
     addcarttomemory();
   }
 };
-clear.addEventListener('click', function() {
-  modal.style.display = 'flex'; 
+clear.addEventListener("click", function () {
+  modal.style.display = "flex";
 });
 
-
-confirmBtn.addEventListener('click', function() {
-  localStorage.clear();  
-  initApp();             
-  location.reload();     
-  modal.style.display = 'none'; 
+confirmBtn.addEventListener("click", function () {
+  localStorage.clear();
+  initApp();
+  location.reload();
+  modal.style.display = "none";
 });
 
-
-cancelBtn.addEventListener('click', function() {
-  modal.style.display = 'none'; 
+cancelBtn.addEventListener("click", function () {
+  modal.style.display = "none";
 });
 
 generateButton.addEventListener("click", function () {
@@ -372,12 +333,7 @@ const initApp = () => {
     })
     .then((data) => {
       listproducts = data;
-      
-      // if (localStorage.getItem("cart")) {
-      //   cart = JSON.parse(localStorage.getItem("cart"));
-      //   addcarttohtml();
-      //   updateCartCount();
-      // }
+
       adddatatohtml();
     })
     .catch((error) => {
